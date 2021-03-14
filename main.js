6cf8f2cd9f6ce6a0a034a7bcf9f06d8ff8695b41
Isabela <im.sarmieento@uniandes.edu.co>
2021-03-14 02:59:15 -0500

    Funcionalidades 3 y 4

--- main.js
+++ main.js
@@ -2,8 +2,19 @@ const dataURL = "'data.json'";
 
 const category_list = document.getElementById("category-list");
 const product_list = document.getElementById("product-list");
+const cartButton = document.getElementById("cart");
 const cart_items = document.getElementById("cart-items");
 const page_title = document.getElementById("page-title");
+
+cartButton.addEventListener(
+  "click",
+  function () {
+    goToCart();
+  },
+  false
+);
+
+var cart = {};
 
 // Obtener los datos y crear la tabla
 fetch("data.json", {
@@ -36,10 +47,10 @@ fetch("data.json", {
       const text = document.createTextNode(element["name"]);
       a.appendChild(text);
     });
+    viewProducts(data[0]["name"], data[0]["products"]);
   });
 
 function viewProducts(category, products) {
-  var html = "";
   product_list.innerHTML = "";
   page_title.innerHTML = "";
 
@@ -112,6 +123,168 @@ function viewProducts(category, products) {
 }
 
 function addToCart(product) {
-  items = parseInt(cart_items.innerHTML[0]) + 1;
+  if (cart[product["name"]] == null)
+    cart[product["name"]] = {
+      product,
+      qty: 0,
+    };
+  cart[product["name"]]["qty"] += 1;
+
+  console.log(cart);
+  items = amountOfItems();
   cart_items.innerHTML = items + " items";
+}
+
+function goToCart() {
+  product_list.innerHTML = "";
+  page_title.innerHTML = "";
+
+  var title = document.createElement("h2");
+  title.classList.add("text-center");
+  text = document.createTextNode("Order detail");
+  page_title.appendChild(title);
+  title.appendChild(text);
+
+  var table = document.createElement("table");
+  table.classList.add("table", "table-striped");
+  product_list.appendChild(table);
+  var head = document.createElement("thead");
+  var body = document.createElement("tbody");
+
+  table.appendChild(head);
+  table.appendChild(body);
+  head.innerHTML =
+    '<tr><th scope="col">Item</th><th scope="col">Qty.</th><th scope="col">Description</th><th scope="col">Unit Price</th><th scope="col">Amount</th><th scope="col">Modify</th></tr>';
+
+  var i = 0;
+  var total = 0;
+  for (const [key, value] of Object.entries(cart)) {
+    console.log(value);
+    i += 1;
+    var newRow = body.insertRow(-1);
+    newRow.setAttribute("id", key);
+
+    price = createRow(newRow, key, value, i);
+
+    total += price;
+  }
+  var table_foot =
+    '<div class="row"><div class="col"><p class="fw-bold">Total:$' +
+    total +
+    '</p></div><div class="col"><div class="float-end"><button class="btn btn-danger">Cancel</button><button class="btn btn-light">Confirmn</button></div></div></div>';
+  var table_end = document.createElement("div");
+  table_end.setAttribute("id", "table_end");
+  table_end.innerHTML = table_foot;
+  product_list.appendChild(table_end);
+}
+
+function amountOfItems() {
+  var am = 0;
+  for (const [key, value] of Object.entries(cart)) {
+    am += value["qty"];
+  }
+  return am;
+}
+
+function addProduct(product_name) {
+  cart[product_name]["qty"] += 1;
+  //console.log(cart);
+  const thisRow = document.getElementById(product_name);
+  addQtyRow(thisRow);
+  const total = calcTotal();
+  const table_foot =
+    '<div class="row"><div class="col"><p class="fw-bold">Total:$' +
+    total +
+    '</p></div><div class="col"><div class="float-end"><button class="btn btn-danger">Cancel</button><button class="btn btn-light">Confirmn</button></div></div></div>';
+  const table_end = document.getElementById("table_end");
+  table_end.innerHTML = table_foot;
+}
+
+function removeProduct(product_name) {
+  cart[product_name]["qty"] -= 1;
+  //console.log(cart);
+  const thisRow = document.getElementById(product_name);
+  rmQtyRow(thisRow);
+  const total = calcTotal();
+  const table_foot =
+    '<div class="row"><div class="col"><p class="fw-bold">Total:$' +
+    total +
+    '</p></div><div class="col"><div class="float-end"><button class="btn btn-danger">Cancel</button><button class="btn btn-light">Confirmn</button></div></div></div>';
+  const table_end = document.getElementById("table_end");
+  table_end.innerHTML = table_foot;
+}
+
+function createRow(newRow, key, value, i) {
+  total = 0;
+  var newCell = newRow.insertCell(-1);
+  var newText = document.createTextNode(i);
+  newCell.appendChild(newText);
+
+  var qty = value["qty"];
+  newCell = newRow.insertCell(-1);
+  newText = document.createTextNode(qty);
+  newCell.appendChild(newText);
+
+  newCell = newRow.insertCell(-1);
+  newText = document.createTextNode(key);
+  newCell.appendChild(newText);
+
+  var price = value["product"]["price"];
+  newCell = newRow.insertCell(-1);
+  newText = document.createTextNode(price);
+  newCell.appendChild(newText);
+
+  newCell = newRow.insertCell(-1);
+  newText = document.createTextNode(price * qty);
+  newCell.appendChild(newText);
+
+  newCell = newRow.insertCell(-1);
+  var add = document.createElement("button");
+  add.innerHTML = "+";
+  add.classList.add("btn", "btn-secondary", "mx-1");
+  add.addEventListener(
+    "click",
+    function () {
+      addProduct(key);
+    },
+    false
+  );
+  var remove = document.createElement("button");
+  remove.innerHTML = "-";
+  remove.classList.add("btn", "btn-secondary", "mx-1");
+  remove.addEventListener(
+    "click",
+    function () {
+      removeProduct(key);
+    },
+    false
+  );
+  newCell.appendChild(add);
+  newCell.appendChild(remove);
+
+  return price * qty;
+}
+
+function addQtyRow(row) {
+  const qty = parseInt(row.cells.item(1).innerHTML);
+  row.cells.item(1).innerHTML = qty + 1;
+  row.cells.item(4).innerHTML =
+    parseFloat(row.cells.item(3).innerHTML) * (qty + 1);
+}
+
+function rmQtyRow(row) {
+  const qty = parseInt(row.cells.item(1).innerHTML);
+  row.cells.item(1).innerHTML = qty - 1;
+  row.cells.item(4).innerHTML =
+    parseFloat(row.cells.item(3).innerHTML) * (qty - 1);
+}
+
+function calcTotal() {
+  var total = 0;
+  for (const [key, value] of Object.entries(cart)) {
+    const price = value["product"]["price"];
+    const qty = value["qty"];
+    total += price * qty;
+  }
+  return total;
 }
